8e4d09654d5db644f568cb5b78e8b9f0a8b363bf
Ruriko Gotou <odoruneko24@gmail.com>
2017-04-17 16:07:48 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,6 +12,7 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
                     <li><a href="#">Signup</a></li>
                     <li><a href="#">Login</a></li>
                 </ul>
